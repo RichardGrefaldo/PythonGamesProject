41b305922e954a9dab65633ec14afb0698dd05b7
Richard Grefaldo <grefaldorichard1021@gmail.com>
2024-01-28 16:16:06 +0800

    Added collision, Name Registration,Moving Cars,

--- TheJayWalker.py
+++ TheJayWalker.py
@@ -1,10 +1,28 @@
 import tkinter as tk
+from tkinter import ttk
+from tkinter import Menu
+from tkinter import messagebox
 root = tk.Tk()
 root.geometry('330x650')
 root.title("The Jay Walker")
-position = 3
-def start():
-    global position
+about = Menu(root)
+root.config(menu=about)
+help_menu = Menu(about,tearoff = 0)
+about.add_cascade(label="Info",menu=help_menu)
+help_menu.add_command(label="About", command=lambda: messagebox.showinfo("About","The Jaywalker by Richard Grefaldo\nVersion 20240128"))
+help_menu.add_command(label="Changelog", command=lambda: messagebox.showinfo("Changelog","Version 20240128\n -Added Moving Cars\n -Added Name Registration\nVersion 20240127\n -Added Collision\n -Added Road Median Strip\n  players now need to jump over \n  the added road median strip \n  in order to cross \nVersion 20240126\n - Initial Release UI only"))
+position,car1pos,car2pos = 5,11,13
+
+
+def Start():
+    global position,playername,npc,car1pos
+    playername = namefield.get().strip()
+    npc = npcfield.get().strip()
+    if playername == "":
+        npc = "Bb"
+        playername = "Jhoe"
+
+
     def trigger_button(event):
         if event.keysym == "a":  # Check if the pressed key is "a"
             leftbutton.invoke()
@@ -14,7 +32,11 @@ def start():
             topbutton.invoke()
         if event.keysym == "s":  # Check if the pressed key is "s"
             downbutton.invoke()
+        if event.keysym == "space":  # Check if the pressed key is "space"
+            jumpbutton.invoke()
+
     tiles = []
+
 
     for i in range(1, 131):
         if i <= 10:
@@ -44,88 +66,251 @@ def start():
         else:
             row = 0
 
-        tile = tk.Button(root,bg="gray", text=i, height=2, width=3)
+        tile = tk.Button(root,bg="gray",text=i, height=2, width=3)
         tile.grid(row=row, column=(i - 1) % 10)
         tiles.append(tile)
+
     t1, t2, t3, t4, t5, t6, t7, t8, t9, t10 = tiles[0], tiles[1], tiles[2], tiles[3], tiles[4], tiles[5], tiles[6], tiles[7], tiles[8], tiles[9]
     t11, t12, t13, t14, t15, t16, t17, t18, t19, t20 = tiles[10], tiles[11], tiles[12], tiles[13], tiles[14], tiles[15],tiles[16], tiles[17], tiles[18], tiles[19]
+    collision = {61:51,62:52,63:53,64:54,65:55,66:56,67:57,68:58,69:59,70:60,131:121,132:122,133:123,134:124,135:125,136:126,137:127,138:128,139:129,140:130} # Handles data for barriers
+    Rcollision = {131:130,121:120,111:110,101:100,91:90,81:80,61:60,51:50,41:40,31:30,21:20,11:10}
+    Lcollision = {0:1,10:11,20:21,30:31,40:41,50:51,60:61,70:71,80:81,90:91,100:101,110:111,120:121}
+    Dcollision = {61:71,62:72,63:73,64:74,65:75,66:76,67:77,68:78,69:79,70:80}
+    carloop = {21:11}
+
+    ###MOVING CARS#####
+    def car1():
+        global car1pos,car2pos
+        car1pos = car1pos + 1
+        car2pos = car2pos + 1
+        print(car1pos)
+        root.after(1000, car1)
+        if car1pos in carloop:
+            car1pos = carloop[car1pos]
+            print(f"position two:{car1pos}")
+        for i, tile in enumerate(tiles):
+            if car1pos == i + 1:
+                tile.config(bg="blue",text=f"Car")
+            elif i == position - 1: # this blocks fixed flickering player's color and name  every time cars move
+                tile.config(bg="red", text=f"{playername}")
+            elif i <= 9:
+                tile.config(bg="green", text="")
+            elif i == 30:
+                tile.config(bg="white", text="")
+            elif i > 32 and i < 36:
+                tile.config(bg="white", text="")
+            elif i > 37 and i < 40:
+                tile.config(bg="white", text="")
+            elif i > 59 and i < 70:
+                tile.config(bg="chocolate")
+            elif i == 90:
+                tile.config(bg="white", text="")
+            elif i > 92 and i < 96:
+                tile.config(bg="white", text="")
+            elif i > 97 and i < 100:
+                tile.config(bg="white", text="")
+            elif i >= 120:
+                tile.config(bg="green", text="")
+
+            else:
+                tile.config(bg="gray", text="")
+
+
+
+    car1()
+
+
+
+
+
+
+
     for i, tile in enumerate(tiles):
         if i <= 9:
             tile.config(bg="green")
-        if i > 37 and i <40:
-            tile.config(bg="white") # color for road lane separator
         if i == 30:
             tile.config(bg="white")
         if i > 32 and i <36:
             tile.config(bg="white")
+        if i > 37 and i <40:
+            tile.config(bg="white") # color for road lane separator
         if i > 59 and i <70:
-            tile.config(bg="chocolate") # color for barrier
-        if i > 92 and i <96:
-            tile.config(bg="white")
+            tile.config(bg="chocolate") # color for road barrier
         if i == 90:
+            tile.config(bg="white")
+        if i > 92 and i <96:
             tile.config(bg="white")
         if i > 97 and i <100:
             tile.config(bg="white")
-
         if i >= 120:
-            tile.config(bg="green")
+            tile.config(bg="green",text=i)
+
     def left(): #function for moving left
         global position
         position = position - 1
         for i, tile in enumerate(tiles):
+            if position in Lcollision:
+                position = Lcollision[position]
             if position == i + 1:
-                tile.config(bg="red")
+                tile.config(bg="red",text=f"{playername}")
             elif i <= 9:        # This part will retain the color green/Grass
-                tile.config(bg="green")
+                tile.config(bg="green",text="")
+            elif i == 30:
+                tile.config(bg="white",text="")
+            elif i > 32 and i < 36:
+                tile.config(bg="white",text="")
+            elif i > 37 and i < 40:
+                tile.config(bg="white",text="")  # color for road lane separator
+            elif i > 59 and i < 70:
+                tile.config(bg="chocolate")
+            elif i == 90:
+                tile.config(bg="white",text="")
+            elif i > 92 and i < 96:
+                tile.config(bg="white",text="")
+            elif i > 97 and i < 100:
+                tile.config(bg="white",text="")
             elif i >= 120:
-                tile.config(bg="green")
+                tile.config(bg="green",text="")
 
             else:
-                tile.config(bg="gray")
+                tile.config(bg="gray",text="")
+
         print(f'{position}')
     def right():
         global position
         position = position + 1
         for i, tile in enumerate(tiles):
+            if position in Rcollision:
+                position = Rcollision[position]  # creates invisible barrier on the right sides
             if position == i + 1:
-                tile.config(bg="red")
+                tile.config(bg="red",text=f"{playername}")
             elif i <= 9:
-                tile.config(bg="green")
-                tile.config(bg="green")
+                tile.config(bg="green",text="")
+            elif i == 30:
+                tile.config(bg="white",text="")
+            elif i > 32 and i < 36:
+                tile.config(bg="white",text="")
+            elif i > 37 and i < 40:
+                tile.config(bg="white",text="")
+            elif i > 59 and i < 70:
+                tile.config(bg="chocolate")
+            elif i == 90:
+                tile.config(bg="white",text="")
+            elif i > 92 and i < 96:
+                tile.config(bg="white",text="")
+            elif i > 97 and i < 100:
+                tile.config(bg="white",text="")
             elif i >= 120:
-                tile.config(bg="green")
+                tile.config(bg="green",text="")
             else:
-                tile.config(bg="gray")
-        print(f'{position}')
+                tile.config(bg="gray",text="")
+            # if position > 60 and position < 71: # Old method
+            # position = position - 1
     def up():
         global position
         position = position + 10
         for i, tile in enumerate(tiles):
             if position == i + 1:
-                tile.config(bg="red")
+                tile.config(bg="red",text=f"{playername}")
             elif i <= 9:
-                tile.config(bg="green")
-                tile.config(bg="green")
+                tile.config(bg="green",text="")
+            elif i == 30:
+                tile.config(bg="white",text="")
+            elif i > 32 and i < 36:
+                tile.config(bg="white",text="")
+            elif i > 37 and i < 40:
+                tile.config(bg="white",text="")
+            elif i > 59 and i < 70:
+                tile.config(bg="chocolate")
+            elif i == 90:
+                tile.config(bg="white",text="")
+            elif i > 92 and i < 96:
+                tile.config(bg="white",text="")
+            elif i > 97 and i < 100:
+                tile.config(bg="white",text="")
             elif i >= 120:
-                tile.config(bg="green")
+                tile.config(bg="green",text="")
             else:
-                tile.config(bg="gray")
-        print(f'{position}')
+                tile.config(bg="gray",text="")
+            if position in collision: # creates barrier  (new method)
+                position = collision[position]
+           # if position > 60 and position < 71:# old method
+               # position = position - 10
+
     def down():
         global position
         position = position - 10
         for i, tile in enumerate(tiles):
+            if position < 1 :  # This will block the player from moving downwards off the game screen
+                position = position + 10
             if position == i + 1:
-                tile.config(bg="red")
+                tile.config(bg="red",text=f"{playername}")
             elif i <= 9:
-                tile.config(bg="green")
+                tile.config(bg="green",text="")
+            elif i == 30:
+                tile.config(bg="white",text="")
+            elif i > 32 and i < 36:
+                tile.config(bg="white",text="")
+            elif i > 37 and i < 40:
+                tile.config(bg="white",text="")
+            elif i > 59 and i < 70:
+                tile.config(bg="chocolate")
+            elif i == 90:
+                tile.config(bg="white",text="")
+            elif i > 92 and i < 96:
+                tile.config(bg="white",text="")
+            elif i > 97 and i < 100:
+                tile.config(bg="white",text="")
             elif i >= 120:
-                tile.config(bg="green")
+                tile.config(bg="green",text="")
             else:
-                tile.config(bg="gray")
+                tile.config(bg="gray",text="")
+            if position in Dcollision:
+                position = Dcollision[position]
+    def jump():
+        global position
+        if position < 51 or position > 70:
+            position = position
+        else:
+            position = position + 20
+
+        for i, tile in enumerate(tiles):
+
+            if position == i + 1:
+                tile.config(bg="red",text=f"{playername}")
+            elif i <= 9:
+                tile.config(bg="green",text="")
+            elif i == 30:
+                tile.config(bg="white",text="")
+            elif i > 32 and i < 36:
+                tile.config(bg="white",text="")
+            elif i > 37 and i < 40:
+                tile.config(bg="white",text="")
+            elif i > 59 and i < 70:
+                tile.config(bg="chocolate",text="")
+            elif i == 90:
+                tile.config(bg="white",text="")
+            elif i > 92 and i < 96:
+                tile.config(bg="white",text="")
+            elif i > 97 and i < 100:
+                tile.config(bg="white",text="")
+            elif i >= 120:
+                tile.config(bg="green",text="")
+            else:
+                tile.config(bg="gray",text="")
+
+
+
+
+
+
+
         print(f'{position}')
 
+
+
     print(f'{position}')
+
 
 
 
@@ -135,19 +320,37 @@ def start():
     leftbutton.place(x=50, y=570, height=20, width=50)
     rightbutton = tk.Button(root,command=right, text="Right")
     rightbutton.place(x=150, y=570, height=20, width=50)
-    topbutton = tk.Button(root, command=up,text="Top")
+    topbutton = tk.Button(root, command=up,text="Up")
     topbutton.place(x=100, y=540, height=20, width=50)
     downbutton = tk.Button(root,command=down, text="Down")
     downbutton.place(x=100, y=600, height=20, width=50)
+    jumpbutton = tk.Button(root,command=jump,text="Jump")
+    jumpbutton.place(x=250,y=540)
     root.bind("<a>", trigger_button)
     root.bind("<w>", trigger_button)
     root.bind("<s>", trigger_button)
     root.bind("<d>", trigger_button)
+    root.bind("<space>", trigger_button)
+    npcfield.destroy()
+    namefield.destroy()
 
+style = ttk.Style()
+style.configure('Custom.TButton', font=('Arial', 15))
+titlelabel = tk.Label(root,text="The Jaywalker",font=('Arial',20))
+titlelabel.place(x=80,y=50)
+titlelabel2 = tk.Label(root,text="by Richard",font=('Arial',14))
+titlelabel2.place(x=120,y=90)
+namelabel = tk.Label(root,text="Enter your name:",font=('Arial',10))
+namelabel.place(x=90,y=150)
+namefield = ttk.Entry(root,font=('Arial',15))
+namefield.place(x=90, y =180,height=40,width=150)
+npclabel = tk.Label(root,text="Enter npc's name:",font=('Arial',10))
+npclabel.place(x=90,y=230)
+npcfield = ttk.Entry(root,font=('Arial',15))
+npcfield.place(x=90, y =250,height=40,width=150)
+startbutton = ttk.Button(root,command=Start,text="Play",style='Custom.TButton')
+startbutton.place(x=140,y=310,height=40,width=60)
 
-
-startbutton = tk.Button(root,command=start,text="Start")
-startbutton.place(x=100,y=100,height=30,width=60)
 
 
 root.mainloop()
